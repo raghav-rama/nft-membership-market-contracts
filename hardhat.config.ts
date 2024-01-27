5c8e5ba44d752fd63818f9bc4c634939e37127f9
raghav-rama <hackerer528@gmail.com>
2024-01-27 18:12:13 +0530

    Setup contract deployment variables

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig({ path: __dirname + "/.env" });
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+if (!ALCHEMY_API_KEY) {
+  throw new Error("ALCHEMY_API_KEY missing");
+}
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+if (!SEPOLIA_PRIVATE_KEY) {
+  throw new Error("SEPOLIA_PRIVATE_KEY missing");
+}
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const nft = await ethers.deployContract("NFT", [
-    "My Token",
-    "MT",
-    "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-  ]);
+  const marketplace = await ethers.deployContract("Marketplace");
 
-  await nft.waitForDeployment();
+  await marketplace.waitForDeployment();
 
-  console.log(`${nft.target}`);
+  console.log(`${marketplace.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
